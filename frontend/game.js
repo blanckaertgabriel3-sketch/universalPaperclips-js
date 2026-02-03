75a751c95e5695feef4315b4f95353c0574d77b5
Tp411 <thomastazetomtom@gmail.com>
2026-02-03 13:58:03 +0100

    click

--- frontend/game.js
+++ frontend/game.js
@@ -1,6 +1,7 @@
 class Game {
   constructor() {
     this.buttonMakePaperclip = document.getElementById("makepaperclip");
+    //the visual area
     this.spanPaperclipNumber = document.getElementById("spanPaperclipNumber");
 
     this.spanUnsoldInventory = document.getElementById("unsoldInventory");
@@ -13,17 +14,20 @@ class Game {
 
     //start with 0 paperclips
     this.paperclipNumber = 0;
-    console.log("the start", this.paperclipNumber);
+    this.initInput();
     this.spanPaperclipNumber.innerHTML = this.paperclipNumber;
-
+  }
+  initInput() {
     //add a listener for add paperclip
-    this.buttonMakePaperclip.addEventListener("click", this.addPaperclip);
+    this.buttonMakePaperclip.addEventListener("click", () => {
+      this.addPaperclip();
+    });
   }
   //indrement paperclip number
   addPaperclip() {
-    console.log("paperclipNumber", this.paperclipNumber);
     this.paperclipNumber++;
-    console.log("paperclipNumber", this.paperclipNumber);
+    this.spanPaperclipNumber.innerHTML = this.paperclipNumber;
   }
 }
-new Game();
+//instanciation of the class Game
+let game1 = new Game();
