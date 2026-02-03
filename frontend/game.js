82066f442627f897e1cb587b416ff7b2687bc333
blanckaertgabriel3-sketch <blanckaertgabriel3@gmail.com>
2026-02-03 12:07:44 +0100

    add peperclips with game class

--- frontend/game.js
+++ frontend/game.js
@@ -1,8 +1,9 @@
 class Game {
 	constructor() {
 		this.buttonMakePaperclip = document.getElementById("makepaperclip");
+		//the visual area
 		this.spanPaperclipNumber = document.getElementById("spanPaperclipNumber");
-
+		
 		this.spanUnsoldInventory = document.getElementById("unsoldInventory");
 		//get elements for Price per clip
 		this.buttonLower = document.getElementById("buttonLower");
@@ -10,20 +11,25 @@ class Game {
 		this.spanPricePerClip = document.getElementById("spanPricePerClip");
 		//get the public demand span
 		this.spanPubicDemand = document.getElementById("spanPubicDemand");
-
+		
 		//start with 0 paperclips
 		this.paperclipNumber = 0;
-		console.log("the start", this.paperclipNumber);
+		this.initInput();
 		this.spanPaperclipNumber.innerHTML = this.paperclipNumber;
-
+	}
+	initInput(){
 		//add a listener for add paperclip
-		this.buttonMakePaperclip.addEventListener("click", this.addPaperclip);
+		this.buttonMakePaperclip.addEventListener("click", () => {
+			this.addPaperclip()
+		});
+		
 	}
 	//indrement paperclip number
 	addPaperclip() {
-		console.log("paperclipNumber", this.paperclipNumber);
 		this.paperclipNumber ++;
-		console.log("paperclipNumber", this.paperclipNumber);
+		this.spanPaperclipNumber.innerHTML = this.paperclipNumber;
+		
 	}
 }
-new Game;
+//instanciation of the class Game
+let game1 = new Game;
