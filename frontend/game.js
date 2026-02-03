31571001aad3bce85439c0cf430cb7f5c1698ee6
Tp411 <thomastazetomtom@gmail.com>
2026-02-03 12:13:54 +0100

    majuscule

--- frontend/game.js
+++ frontend/game.js
@@ -1,35 +1,29 @@
 class Game {
-	constructor() {
-		this.buttonMakePaperclip = document.getElementById("makepaperclip");
-		//the visual area
-		this.spanPaperclipNumber = document.getElementById("spanPaperclipNumber");
-		
-		this.spanUnsoldInventory = document.getElementById("unsoldInventory");
-		//get elements for Price per clip
-		this.buttonLower = document.getElementById("buttonLower");
-		this.buttonRaise = document.getElementById("buttonRaise");
-		this.spanPricePerClip = document.getElementById("spanPricePerClip");
-		//get the public demand span
-		this.spanPubicDemand = document.getElementById("spanPubicDemand");
-		
-		//start with 0 paperclips
-		this.paperclipNumber = 0;
-		this.initInput();
-		this.spanPaperclipNumber.innerHTML = this.paperclipNumber;
-	}
-	initInput(){
-		//add a listener for add paperclip
-		this.buttonMakePaperclip.addEventListener("click", () => {
-			this.addPaperclip()
-		});
-		
-	}
-	//indrement paperclip number
-	addPaperclip() {
-		this.paperclipNumber ++;
-		this.spanPaperclipNumber.innerHTML = this.paperclipNumber;
-		
-	}
+  constructor() {
+    this.buttonMakePaperclip = document.getElementById("makepaperclip");
+    this.spanPaperclipNumber = document.getElementById("spanPaperclipNumber");
+
+    this.spanUnsoldInventory = document.getElementById("unsoldInventory");
+    //get elements for Price per clip
+    this.buttonLower = document.getElementById("buttonLower");
+    this.buttonRaise = document.getElementById("buttonRaise");
+    this.spanPricePerClip = document.getElementById("spanPricePerClip");
+    //get the public demand span
+    this.spanPubicDemand = document.getElementById("spanPubicDemand");
+
+    //start with 0 paperclips
+    this.paperclipNumber = 0;
+    console.log("the start", this.paperclipNumber);
+    this.spanPaperclipNumber.innerHTML = this.paperclipNumber;
+
+    //add a listener for add paperclip
+    this.buttonMakePaperclip.addEventListener("click", this.addPaperclip);
+  }
+  //indrement paperclip number
+  addPaperclip() {
+    console.log("paperclipNumber", this.paperclipNumber);
+    this.paperclipNumber++;
+    console.log("paperclipNumber", this.paperclipNumber);
+  }
 }
-//instanciation of the class Game
-let game1 = new Game;
+new Game();
